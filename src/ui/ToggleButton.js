df479b90159f72290fb7acf1a8071d8c6ba1f94e
jsoto3000 <33795239+jsoto3000@users.noreply.github.com>
2018-11-13 14:03:26 -0500

    Added tabindex to togglebutton

--- src/ui/ToggleButton.js
+++ src/ui/ToggleButton.js
@@ -44,7 +44,7 @@ export class ToggleButton extends React.Component {
 
         if (this.isToggled()) {
             return (
-                <div onClick={this.handleClick} className="sb-toggle on">
+                <div onClick={this.handleClick} className="sb-toggle on" tabindex="0">
                 <i className={this.getIconClass()}></i>
                 <span> {this.props.text}</span>
                 </div>
@@ -52,7 +52,7 @@ export class ToggleButton extends React.Component {
         }
 
         return (
-        <div onClick={this.handleClick} className="sb-toggle off">
+        <div onClick={this.handleClick} className="sb-toggle off" tabindex="-1">
         <i className={this.getIconClass()}></i>
         <span> {this.props.text}</span>
         </div>
