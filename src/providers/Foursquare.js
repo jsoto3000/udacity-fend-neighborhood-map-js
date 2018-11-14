ca32919d315c7e24b1eb6c98a7dd49175782a300
jsoto3000 <33795239+jsoto3000@users.noreply.github.com>
2018-11-14 00:42:56 -0500

    Updated CS and added .catch with alert

--- src/providers/Foursquare.js
+++ src/providers/Foursquare.js
@@ -2,9 +2,9 @@
 // Provides the basic methods for accessing the API
 
 
-const URL = 'https://api.foursquare.com/v2/';
+const URL = 'https://api.foursquare.com/v2';
 const CLIENT_ID = 'ZRKYGFU3YZO1ENL0N5CDZVX3XFSRNUFEHREBBKHRMF5UZ3AG';
-const CLIENT_SECRET = 'YSPM1AGFO5TYTTQXV1ENDYZV3OZE3TFJFBXUGO1Z1KSTY4UD';
+const CLIENT_SECRET = 'KNTXOJBYFTHDQBEVXJ2INGFLPXZJIZKK2SF2VC25CI4EEZZM';
 
 const headers = {
     'Accept': 'application/json',
@@ -14,7 +14,9 @@ const headers = {
 export const getDetail = (id) => 
     fetch(`${URL}/venues/${id}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=20180510`, {...headers})
     .then(res => res.json())
-    .then(data => data);
+    .then(data => data)
+    .catch(error => {alert('Error while getting location data from FourSquare API')});
+
 
 export const searchFor = (query, lat, lng) => 
     fetch(`${URL}/venues/search?query=${query}&ll=${lat},${lng}`, {...headers})
